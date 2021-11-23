cda014ff5b0723764eb4622c69b83b7d2db80f6d
sc0der <siriusproger@gmail.com>
2021-11-23 23:03:05 +0500

    added line class

--- adapter.py
+++ adapter.py
@@ -12,3 +12,8 @@ class Line:
     def __init__(self, start, end):
         self.start = start
         self.end = end
+
+class Rectangle(list):
+    def __init__(self,x, y, width, height):
+        super().__init__()
+        self.append(Line(Point(x, y),Point( x+width, y)))
