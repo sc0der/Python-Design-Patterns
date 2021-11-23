e5dd16f4fbc3e5b278bf1f1ea637f4524e07f8b8
sc0der <siriusproger@gmail.com>
2021-11-23 23:16:03 +0500

    added line class

--- adapter.py
+++ adapter.py
@@ -1,5 +1,4 @@
 class Point:
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -17,3 +16,28 @@ class Rectangle(list):
     def __init__(self,x, y, width, height):
         super().__init__()
         self.append(Line(Point(x, y),Point( x+width, y)))
+        self.append(Line(Point(x+width, y),Point( x+width, y+height)))
+        self.append(Line(Point(x, y),Point( x, y+height)))
+        self.append(Line(Point(x, y+height),Point( x+width, y+height)))
+
+class LineToPointAdapter(list):
+    count = 0
+
+    def __init__(self,line):
+        super().__init__()
+        self.count += 1
+        print(
+            f'{self.count}: Generating points for line'
+            f'[{line.start.x},{line.start.y}]'
+            f'[{line.end.x},{line.end.y}]'
+        )
+        left = min(line.start.x, line.end.x)
+        right = max(line.start.x, line.end.x)
+        top = min(line.start.y, line.end.y)
+        bottom = min(line.start.y, line.end.y)
+
+def draw(rcs):
+    print("\n\n--- Drawing some stuff ---\n")
+    for rc in rcs:
+        for line in rcs:
+            pass
