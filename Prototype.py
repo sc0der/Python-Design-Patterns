a762315e4ededfd58a86862e98ae64ab7909534e
sc0der <siriusproger@gmail.com>
2021-12-06 18:51:48 +0500

    added class Adress

---
+++ Prototype.py
@@ -0,0 +1,8 @@
+class Address:
+    def __init__(self, street_address, city, country):
+        self.street_address = street_address
+        self.city = city
+        self.country = country
+
+    def __str__(self):
+        return f'{self.street_address}, {self.city}, {self.country}'
