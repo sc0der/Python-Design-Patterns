36b4428ddae9cefadeabb7cb8ad7a2aa21355548
sc0der <siriusproger@gmail.com>
2021-12-06 18:53:46 +0500

    added str method to the \ class Person

--- Prototype.py
+++ Prototype.py
@@ -11,4 +11,5 @@ class Person:
     def __init__(self,name, address):
         self.name = name
         self.address = address
-        
+    def __str__(self):
+        return f'{self.name}, lives at {self.address}'
