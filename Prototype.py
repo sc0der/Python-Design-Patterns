8d3660932bd43db6f881329c62a73cc4ce5fbf08
sc0der <siriusproger@gmail.com>
2021-12-06 22:12:29 +0500

    create the references

--- Prototype.py
+++ Prototype.py
@@ -1,3 +1,4 @@
+import copy
 class Address:
     def __init__(self, street_address, city, country):
         self.street_address = street_address
@@ -7,9 +8,18 @@ class Address:
     def __str__(self):
         return f'{self.street_address}, {self.city}, {self.country}'
 
+
 class Person:
-    def __init__(self,name, address):
+    def __init__(self, name, address):
         self.name = name
         self.address = address
+
     def __str__(self):
         return f'{self.name}, lives at {self.address}'
+
+
+john = Person("John", Address("123 Main Street", 'London', "UK"))
+jane = copy.deepcopy(john)
+jane.name = "Jane"
+jane.address.street_address = "Jane"
+print(jane, '\n', john)
