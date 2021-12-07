2feafda0da077f7c9cb0b7427febe2ba71c10644
sc0der <siriusproger@gmail.com>
2021-12-07 23:54:19 +0500

    added new class

--- SingeltonExample.py
+++ SingeltonExample.py
@@ -5,3 +5,12 @@ class Singleton:
         if cls not in cls._instances:
             cls.__instances[cls] = super(Database, cls).__call__(cls, *a, **kw)
         return cls.__instances[cls]
+
+class Database:
+    def __init__(self):
+        self.populations = {}
+        f = open("capitals.txt", 'r')
+        lines = f.readlines()
+        for i in range(1, len(lines), 2):
+            self.populations[lines[i].strip()] = int(lines[i+1].strip())
+        f.close()
