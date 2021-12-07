0633e3d630fffe4b0fd3c12fcf53076a1ea7b50a
sc0der <siriusproger@gmail.com>
2021-12-07 23:59:41 +0500

    added unit testing class

--- SingeltonExample.py
+++ SingeltonExample.py
@@ -1,4 +1,6 @@
+import unittest
 class Singleton:
+
     _instances = {}
 
     def __call__(cls, *a, **kw):
@@ -21,5 +23,11 @@ class SingletonRecordFinder:
         for c in cities:
             result += Database().population[c]
         return result
+
+class SingletonTest(unittest.TestCase):
+    def test_is_singleton(self):
+        db1 = Database()
+        db2 = Database()
+        self.assertEqual(db1, db2)
 
         
